6a7d55db6147f7c0cab8fb00913007e575ebeadb
PsuperX <ja435364@gmail.com>
2025-04-24 11:21:02 +0100

    release ball

--- controllers/supervisor_controller/supervisor_controller.py
+++ controllers/supervisor_controller/supervisor_controller.py
@@ -8,6 +8,8 @@ from controller import PositionSensor, Motor, Supervisor
 
 JOINT_NAMES = [f"joint{i}" for i in range(1, 6)]
 JOINT_SENSOR_NAMES = [f"joint{i}_sensor" for i in range(1, 6)]
+HAND_NAMES = [f"finger_{i}_joint_1" for i in [1, 2, "middle"]]
+HAND_SENSOR_NAMES = [f"finger_{i}_joint_1_sensor" for i in [1, 2, "middle"]]
 
 JOINT_LIMITS = [
     (-2.792, 2.792),
@@ -19,7 +21,6 @@ JOINT_LIMITS = [
 ]
 
 # TODO:
-# - rewards
 # - parar o robo depois de lançar e só simular bola?
 
 class BallerSupervisor(RobotSupervisorEnv):
@@ -33,6 +34,7 @@ class BallerSupervisor(RobotSupervisorEnv):
         self.hoop = self.getFromDef("HOOP")
         self.ball = self.getFromDef("BALL")
 
+        # Joint Motors
         self.joints = []
         for joint_name in JOINT_NAMES:
             joint: Motor = self.getDevice(joint_name)
@@ -40,11 +42,27 @@ class BallerSupervisor(RobotSupervisorEnv):
             joint.setVelocity(0)
             self.joints.append(joint)
 
+        # Joint Sensors
         self.joint_sensors = []
         for joint_name in JOINT_SENSOR_NAMES:
             sensor: PositionSensor = self.getDevice(joint_name)
             sensor.enable(self.timestep)
             self.joint_sensors.append(sensor)
+
+        # Hand Motors
+        self.hand = []
+        for hand_name in HAND_NAMES:
+            joint: Motor = self.getDevice(hand_name)
+            joint.setPosition(float("inf"))
+            joint.setVelocity(0)
+            self.hand.append(joint)
+
+        # Hand Sensors
+        self.hand_sensors = []
+        for hand_name in HAND_SENSOR_NAMES:
+            sensor: PositionSensor = self.getDevice(hand_name)
+            sensor.enable(self.timestep)
+            self.hand_sensors.append(sensor)
 
         self.was_higher_than_hoop = False
         self.ball_last_vel = np.array([0, 0, 0])
@@ -53,12 +71,10 @@ class BallerSupervisor(RobotSupervisorEnv):
         self.passed_hoop = False # ball has passed the hoop
         self.closest_dist = 99999
         self.passing_center = self.hoop.getPosition()
-        # Access the geometry node of the hoop
-        geometry = self.hoop.getField("children").getMFNode(0).getField("geometry")
         # Get the outer radius (distance from center to tube middle)
-        outer_radius = geometry.getField("outerRadius").getSFFloat()
+        outer_radius = self.hoop.getField("majorRadius").getSFFloat()
         # Get the inner radius (tube thickness)
-        inner_radius = geometry.getField("innerRadius").getSFFloat()
+        inner_radius = self.hoop.getField("minorRadius").getSFFloat()
         self.passing_radius = outer_radius-inner_radius
 
         self.steps_per_episode = 200  # Max number of steps per episode
@@ -75,7 +91,8 @@ class BallerSupervisor(RobotSupervisorEnv):
         # Relative pos
         relative_pos = [hoop_pos[i] - ball_pos[i] for i in range(len(hoop_pos))]
         #################
-        if self.closest_dist > min(relative_pos): self.closest_dist = relative_pos # see if there is a way to improve this
+        if self.closest_dist > sum(rv**2 for rv in relative_pos):
+            self.closest_dist = sum(rv**2 for rv in relative_pos) # see if there is a way to improve this
         #################
         obs.extend(relative_pos)
 
@@ -98,12 +115,7 @@ class BallerSupervisor(RobotSupervisorEnv):
         return [0.0 for _ in range(self.observation_space.shape[0])]
 
     def get_reward(self, action=None):
-        # Reward is +1 for every step the episode hasn't ended
-        return 1
-
-    def reward(self):
-        # Called after simulation to evaluate the run
-        return self.released * (999 * self.passed_hoop + self.closest_dist)
+        return self.released_ball * (999 * self.passed_hoop + self.closest_dist)
 
     def is_ball_passing(self):
         ball_center = self.ball.getPosition() #returns x,y,z coordenates of center
@@ -119,6 +131,9 @@ class BallerSupervisor(RobotSupervisorEnv):
 
     def is_done(self):
         ball_pos = self.ball.getPosition()
+
+        if self.is_ball_passing():
+            return True
 
         if self.was_higher_than_hoop and ball_pos[2] < self.hoop.getPosition()[2]:
             # Was high enough
@@ -146,16 +161,26 @@ class BallerSupervisor(RobotSupervisorEnv):
         if self.ball.getPosition()[2] >= self.hoop.getPosition()[2]:
             self.was_higher_than_hoop = True
 
+        ball_pos = self.ball.getPosition()
+        hand_pos = self.getFromDef("HAND").getPosition()
+        if 0.2 < np.sqrt((ball_pos[0] - hand_pos[0]) ** 2 +
+                    (ball_pos[1] - hand_pos[1]) ** 2 +
+                    (ball_pos[2] - hand_pos[2]) ** 2):
+            self.released_ball = True
+
         for i in range(len(self.joints)):
             self.joints[i].setPosition(float("inf"))
             self.joints[i].setVelocity(action[i])
 
         release_ball = action[-1]
-        # TODO: release the ball
+        for i in range(len(self.hand)):
+            self.hand[i].setPosition(float("inf"))
+            self.hand[i].setVelocity(release_ball)
 
     def reset(self):
         self.was_higher_than_hoop = False
         self.ball_last_vel = np.array([0, 0, 0])
+        self.released_ball = False
 
         return super().reset()
 
